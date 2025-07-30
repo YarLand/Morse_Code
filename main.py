dd620e192129db4615d40afb292e16d7b261bf8d
YarLand <137598161+YarLand@users.noreply.github.com>
2025-07-30 18:45:33 +0300

    Fixed and Implemented Proper Decoder

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from morse_parser import MorseParser
 morse_parser = MorseParser()
 
 morse_parser.encode("hi lol")
+
+morse_parser.decode(".... .. / .-.. --- .-..")
--- morse_parser.py
+++ morse_parser.py
@@ -2,14 +2,15 @@ from morse_dict import MorseDict
 
 morse_dict = MorseDict()
 
-morse_dict.MorseInit()
-
-print(morse_dict.m_dict)
-
 class MorseParser:
     def __init__(self):
-        pass
+        morse_dict.MorseInit()
 
     def encode(self, message):
         encoded = " ".join([morse_dict.m_dict[letter.lower()] for letter in message]).strip()
         print(encoded)
+
+    def decode(self, message):
+        flipped_dict = {value: key for key, value in morse_dict.m_dict.items()}
+        decoded = "".join([flipped_dict[block] for block in message.split(" ")]).strip()
+        print(decoded)
