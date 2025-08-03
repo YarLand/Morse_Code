0149df6396df0e3b541345509ba28195c3c2f156
YarLand <137598161+YarLand@users.noreply.github.com>
2025-08-03 21:27:06 +0300

    Implemented Message Proof, Consistent Menu, More Options in Menu

--- main.py
+++ main.py
@@ -2,6 +2,28 @@ from morse_parser import MorseParser
 
 morse_parser = MorseParser()
 
-morse_parser.encode("hi lol")
+def define_message():
+    message_text = input("Please enter the message:\n")
+    return message_text
 
-morse_parser.decode(".... .. / .-.. --- .-..")
+while True:
+    menu_main_choice = input("Would you like to (E)ncode or (D)ecode Morse Code?\n"
+                             "Please press the key relating to the first letter of the command.\n"
+                             "Additional: e(X)it, (V)iew supported characters\n")
+
+    match menu_main_choice.upper():
+        case "D":
+            morse_parser.message_proof(morse_parser.decode,define_message())
+        case "E":
+            morse_parser.message_proof(morse_parser.encode,define_message())
+        case "V":
+            morse_parser.debug_show_dict()
+        case "X":
+            break
+        case _:
+            print("Invalid Choice, please try again.\n")
+
+
+# morse_parser.encode("hi lol")
+#
+# morse_parser.decode(".... .. / .-.. --- .-..")
--- morse_parser.py
+++ morse_parser.py
@@ -3,8 +3,17 @@ from morse_dict import MorseDict
 morse_dict = MorseDict()
 
 class MorseParser:
+
     def __init__(self):
         morse_dict.MorseInit()
+
+    def message_proof(self, func, message):
+        for character in message:
+            if character not in morse_dict.m_dict:
+                print(f"Message contains invalid character: {character}.\n"
+                      f"Please remove the characters from the message")
+                return None
+        return func(message)
 
     def encode(self, message):
         encoded = " ".join([morse_dict.m_dict[letter.lower()] for letter in message]).strip()
@@ -14,3 +23,6 @@ class MorseParser:
         flipped_dict = {value: key for key, value in morse_dict.m_dict.items()}
         decoded = "".join([flipped_dict[block] for block in message.split(" ")]).strip()
         print(decoded)
+
+    def debug_show_dict(self):
+        print(morse_dict.m_dict)
