c186a40ef959832df18ef333bb92a9738e084940
YarLand <137598161+YarLand@users.noreply.github.com>
2025-08-26 23:39:53 +0300

    Added "Debug" quiz file, Added User score, with a results screen.

--- morse_quiz.py
+++ morse_quiz.py
@@ -8,15 +8,25 @@ morse_parser = MorseParser()
 class MorseQuiz:
 
     def __init__(self):
-        self.m_quiz = morse_dict.MorseInit("morse_quiz.tsv")
+        self.m_quiz = morse_dict.MorseInit("morse_quiz_debug.tsv")
 
 
     def morse_quiz_run(self):
-        for answer, question in random.sample(list(self.m_quiz.items()),len(list(self.m_quiz.items()))):
+        score_max = len(list(self.m_quiz.items()))
+        score_user = 0
+        for answer, question in random.sample(list(self.m_quiz.items()),score_max):
             player = input(f"Decode the following Morse Code:\n{question}\nenter (X) to exit\n")
             if player.lower() == answer:
-                print(f"Correct! It was: '{answer}'!\n")
+                print(f"Correct! It was: '{answer}'!")
+                score_user += 1
             elif player.lower == "x":
                 break
             else:
-                print(f"Incorrect, It was: '{answer}'.\n")
+                print(f"Incorrect, It was: '{answer}'.")
+            print(f"Current Score: {score_user}\n")
+        print("Quiz Over!\n"
+              f"You got {score_user} out of {score_max} correct!")
+        if score_user == score_max:
+            print("A perfect score! Good job!\n")
+        elif score_user < score_max:
+            print("Try aiming for a perfect score, Good luck!\n")
