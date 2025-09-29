a14d0ece5eba35ed1b8642c61836c8b34e439294
YarLand <137598161+YarLand@users.noreply.github.com>
2025-09-29 19:25:14 +0300

    Quiz: * Changed name "quiz_guess" into "quiz_input" to better clarify the answering style. * Added Comments

--- Modules/morse_quiz.py
+++ Modules/morse_quiz.py
@@ -5,106 +5,189 @@ from Modules.morse_quiz_assist import MorseQuizAssist
 
 class MorseQuiz:
     def __init__(self):
+        # Dictionary that stores the quiz files per difficulty name
         self.level_dict = {'Easy': 'morse_quiz_easy.tsv',
                            'Medium': 'morse_quiz_medium.tsv',
                            'Hard': 'morse_quiz_hard.tsv',
                            'Expert': 'morse_quiz_expert.tsv'}
 
     def morse_quiz_run(self,morse_settings):
+        # Runs the quiz program
         while True:
+            # Displays the difficulty levels
             print("Please select difficulty level:")
+            # Since ranges are 0-index,
+            # We start with 1 for easier selection
+            # in the number row on the keyboard
             i = 1
+            # For each level,
+            # display the selection number
+            # and associated difficulty
             for key in self.level_dict:
                 print(f"({i}) {key}")
                 i += 1
+            # Prompt difficulty selection
             difficulty = input("\nType the number corresponding to the level:\n")
+            # Check if input is a number,
+            # since we want to convert string to integer for comparison
             if difficulty.isnumeric():
+                # Convert string to integer number
                 difficulty = int(difficulty)
+                # If the input is in range,
+                # break the loop process and proceed
                 if 1 <= difficulty <= len(self.level_dict):
                     break
+            # If the input is incorrect,
+            # display error and repeat loop
             print("Incorrect input, Please try again:")
 
         while True:
-            quiz_style= input("Would you like the questions to:\n"
-                              "(1) have options.\n"
-                              "(2) guess directly?\n")
+            # Quiz style selection
+            quiz_style= input("Choose answer format:\n"
+                              "(1) Options\n"
+                              "(2) Input\n")
             match quiz_style:
+                # Options Style
                 case "1":
                     quiz_options(self,difficulty,morse_settings)
                     break
+                # Input Style
                 case "2":
-                    quiz_guess(self,difficulty,morse_settings)
+                    quiz_input(self, difficulty, morse_settings)
                     break
+                # Invalid Input
                 case _:
                     print("Invalid input, Try again.")
 
 def quiz_options(self,difficulty,morse_settings):
+    # Initiates the quiz assist module,
+    # which facilitates common quiz functions.
     quiz_assist = MorseQuizAssist(difficulty,self.level_dict)
+    # Randomly pick a question and answer
+    # from the selected difficulty dictionary
     for answer, question in random.sample(list(quiz_assist.m_quiz.items()),
                                           quiz_assist.score_max):
-        options_list = [item[0] for item in random.sample(list(quiz_assist.m_quiz.items()),
-                                                       4)]
+        # Loads 4 random answers to the guesses
+        options_list = [item[0] for item
+                        in random.sample(list(quiz_assist.m_quiz.items()),4)]
+        # If none of the 4 answers loaded
+        # relates to the current question,
+        # Then replace the first guess with
+        # the correct answer
         if not answer in options_list:
             options_list[0] = answer
+        # Question Loop
         while True:
+            # Converts morse question into the current format
+            # and displays to the user
             question = question.translate(str.maketrans(".-/",
                                                         f"{morse_settings["dot"]}"
                                                         f"{morse_settings["dash"]}"
                                                         f"{morse_settings["slash"]}"))
             print(question)
+
+            # Since ranges are 0-index,
+            # We start with 1 for easier selection
+            # in the number row on the keyboard
             i = 1
+            # For each option,
+            # display the selection number
+            # and associated answer
             for key in options_list:
                 print(f"({i}) {key}")
                 i += 1
             print(f"Enter (exit) to exit.\n")
+            # Prompt the user for option selection
             user_guess = input("Please select an answer:\n")
+            # if the input is "exit" then quit the quiz.
             if user_guess == "exit":
                 break
+            # Check if input is a number,
+            # since we want to convert string to integer for comparison
             if user_guess.isnumeric():
+                # Convert string to integer number
                 user_guess = int(user_guess)
+                # Check if the input is in range
                 if 1 <= user_guess <= len(options_list):
+                    # If the option was the correct answer
                     if options_list[user_guess-1] == answer:
+                        # Then the player is rewarded a point
                         quiz_assist.correct(answer)
                         break
+                    # If it was incorrect
                     else:
+                        # Then the user will be shown the correct answer
+                        # and the error is added to a list of wrong answers
                         quiz_assist.incorrect(answer,question)
                         break
+            # If the input was invalid, restart the current loop
             print("Incorrect input, Please try again:")
+        # if the input is "exit" then quit the quiz.
+        # It is needed a second time due to 2 loops.
         if user_guess == "exit":
             break
+        # Displays the current score
         quiz_assist.current_score()
+    # Displays that the quiz is done
+    # and shows the score and errors of the player.
     quiz_assist.quiz_finish()
 
 
-def quiz_guess(self,difficulty,morse_settings):
+def quiz_input(self, difficulty, morse_settings):
+    # Initiates the quiz assist module,
+    # which facilitates common quiz functions.
     quiz_assist = MorseQuizAssist(difficulty,self.level_dict)
+    # Randomly pick a question and answer
+    # from the selected difficulty dictionary
     for answer, question in random.sample(
             list(quiz_assist.m_quiz.items()),
             quiz_assist.score_max):
+        # Question Loop
         while True:
+            # Converts morse question into the current format
+            # and displays to the user
             question = question.translate(str.maketrans(".-/",
                                           f"{morse_settings["dot"]}"
                                           f"{morse_settings["dash"]}"
                                           f"{morse_settings["slash"]}"))
+            # Prompt the user for decoding input
             player = input(f"Decode the following Morse Code\n"
                            f"(Letters, Numbers and Spaces only):\n"
                            f"{question}\n"
                            f"Enter (exit) to exit.\n").lower()
+            # Potential list of invalid characters.
             error_chars = []
+            # For each character in input
             for char in player:
+                # check if it isn't a character,
+                # number or space
                 if not (char.isalnum() or char.isspace()):
+                    # If it isn't add to the error characters list
                     error_chars.append(char)
+            # If the entire input message
+            # passed the check, proceed with the quiz.
             if not error_chars:
                 break
+            # If there was one or more invalid characters,
+            # Then show the user these characters and repeat the loop
             else:
                 print(f"The following character/s are invalid:\n"
                       f"{set(error_chars)}\n"
                       f"Try again.")
+        # If the input was the correct answer
         if player == answer:
+            # Then the player is rewarded a point
             quiz_assist.correct(answer)
+        # if the input is "exit" then quit the quiz.
         elif player == "exit":
             break
+        # If it was incorrect
         else:
+            # Then the user will be shown the correct answer
+            # and the error is added to a list of wrong answers
             quiz_assist.incorrect(answer, question)
+        # Displays the current score
         quiz_assist.current_score()
+    # Displays that the quiz is done
+    # and shows the score and errors of the player.
     quiz_assist.quiz_finish()
